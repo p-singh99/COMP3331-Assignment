44d980782e712d29e0b2b41cfdd70298bc31a09b
Pawanjot Singh <singh.pawanjot23@gmail.com>
2021-08-04 02:05:06 +1000

    changes

--- src/receiver/receiver.py
+++ src/receiver/receiver.py
@@ -1,4 +1,30 @@
 import sys
+from socket import *
+
+class Receiver:
+    def __init__(self, receiverPort, fileReceive):
+        self.receiverPort = receiverPort
+        self.fileReceive = fileReceive
+        self.receiverSocket = None
+
+    def setup_connection(self):
+        try:
+            self.receiverSocket = socket(AF_INET, SOCK_DGRAM)
+        except:
+            print('Error creating a Receiver socket.')
+            sys.exit()
+
+        try:
+            self.receiverSocket.bind(('localhost', self.receiverPort))
+        except:
+            print(f'Error binding to port - {self.receiverPort}')
+            sys.exit()
+
+    def listen(self):
+        while 1:
+            msg, serverAddress = self.receiverSocket.recvfrom(64000)
+            print(f'Received data: {msg.decode()}')
+
 
 def check_args(args):
     if len(args) != 3:
@@ -7,6 +33,11 @@ def check_args(args):
 
 if __name__ == '__main__':
     if check_args(sys.argv):
-        print('Valid Receiver')
+        
+        # Setup Socket
+        receiver = Receiver(int(sys.argv[1]), sys.argv[2])
+        receiver.setup_connection()
+        receiver.listen()
+
     else:
         print('Receiver Arguments not Valid')
--- src/sender/sender.py
+++ src/sender/sender.py
@@ -1,15 +1,25 @@
+from socket import AF_INET, SOCK_DGRAM
 import sys
+from socket import *
 
 class Sender:
     def __init__(self, argv):
         self.recvHost = argv[1]
-        self.recvPort = argv[2]
+        self.recvPort = int(argv[2])
         self.file = argv[3]
-        self.MWS = argv[4]
-        self.MSS = argv[5]
-        self.timeout = argv[6]
-        self.pDrop = argv[7]
-        self.seed = argv[8]
+        self.MWS = int(argv[4])
+        self.MSS = int(argv[5])
+        self.timeout = int(argv[6])
+        self.pDrop = int(argv[7])
+        self.seed = int(argv[8])
+        self.senderSocket = None
+
+    def setup_connection(self):
+        try:
+            self.senderSocket = socket(AF_INET, SOCK_DGRAM)
+        except:
+            print('Error creating Sender socket')
+            sys.exit()
 
 def check_args(args):
     if len(args) != 9:
@@ -18,7 +28,9 @@ def check_args(args):
 
 if __name__ == "__main__":
     if check_args(sys.argv):
-        print('Valid Arguments')
         sender = Sender(sys.argv)
+        sender.setup_connection()
+        msg = 'Hello Receiver'
+        sender.senderSocket.sendto(msg.encode(), ('localhost', sender.recvPort))
     else:
         print('Arguments not Valid');
